07bef54af99beea6509f1c486e4f3b4c585067a1
Ezha Randall <ezha.randall@gmail.com>
2017-10-05 17:05:16 -0400

    added mock products

--- bamazon/bamazon.sql
+++ bamazon/bamazon.sql
@@ -12,3 +12,34 @@ CREATE TABLE products (
 );
 
 select * from products;
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('John Wall Jersey', 'Sportswear', 150.00, 40);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Bradley Beal Jersey', 'Sportswear', 150.00, 40);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Kirk Cousins Jersey', 'Sportswear', 250.00, 30);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Josh Norman Jersey', 'Sportswear', 250.00, 25);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Football', 'Sporting Goods', 25.00, 100);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Basketball', 'Sporting Goods', 25.00, 100);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Gatorade: Assorted Flavors', 'Drinks', 30.00, 40);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Small Knee Pads','Sporting Goods', 30.00, 40);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Medium Knee Pads','Sporting Goods', 30.00, 40);
+
+INSERT INTO products (product_name, department_name , price, stock_quantity)
+VALUES ('Large Knee Pads','Sporting Goods', 30.00, 40);
+
