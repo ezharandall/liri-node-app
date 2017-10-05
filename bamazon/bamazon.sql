81368559174961ac3f0c93f0cc18ba1dfd46ccaf
Ezha Randall <ezha.randall@gmail.com>
2017-10-05 16:49:56 -0400

    adding products table to database per instructions

---
+++ bamazon/bamazon.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
+);
+
+select * from products;
